c0d846c51329ef08d97652f80a27a91fa7a619d1
Evaldo Junior <evalldojn@gmail.com>
2022-05-12 22:20:31 -0300

    Remover erros

---
+++ src/test/java/randoop/bin/RegressionTest.java
@@ -0,0 +1,225 @@
+package randoop.bin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+
+public class RegressionTest {
+    @Test
+    public void test1() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        int int3316 = calculadora905.mulInt(1000, -500);
+        Assertions.assertEquals(int3316, -500000);
+    }
+
+    @Test
+    public void test3() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+    }
+
+    @Test
+    public void test4() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+    }
+
+    @Test
+    public void test5() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+    }
+
+    @Test
+    public void test6() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+        boolean boolean448 = calculadora905.menor(-1000, 1000);
+        Assertions.assertEquals(boolean448, true);
+    }
+
+    @Test
+    public void test7() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+        boolean boolean2687 = calculadora905.maior(500, 500);
+        Assertions.assertEquals(boolean2687, false);
+    }
+
+    @Test
+    public void test8() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        int int34 = calculadora905.divInt(100, -100);
+        Assertions.assertEquals(int34, -1);
+    }
+
+    @Test
+    public void test9() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+        boolean boolean7027 = calculadora905.menor(-1000, -100);
+        Assertions.assertEquals(boolean7027, true);
+    }
+
+    @Test
+    public void test10() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+        boolean boolean7027 = calculadora905.menor(-1000, -100);
+        Assertions.assertEquals(boolean7027, true);
+        double double9535 = calculadora905.somaDouble(3.14159, -1.55);
+        Assertions.assertEquals(double9535, 1.5915899999999998);
+    }
+
+    @Test
+    public void test11() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        int int3316 = calculadora905.mulInt(1000, -500);
+        Assertions.assertEquals(int3316, -500000);
+        boolean boolean3099 = calculadora905.menor(250, -100);
+        Assertions.assertEquals(boolean3099, false);
+    }
+
+    @Test
+    public void test12() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean7857 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean7857, false);
+        boolean boolean7027 = calculadora905.menor(-1000, -100);
+        Assertions.assertEquals(boolean7027, true);
+        double double4189 = calculadora905.somaDouble(0.55, -1.55);
+        Assertions.assertEquals(double4189, -1.0);
+    }
+
+    @Test
+    public void test13() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        int int5805 = calculadora905.somaInt(-1000, 100);
+        Assertions.assertEquals(int5805, -900);
+    }
+
+    @Test
+    public void test14() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        java.lang.String string1288 = calculadora905.toLowCase("lOrEm");
+        Assertions.assertEquals(string1288, "lorem");
+    }
+
+    @Test
+    public void test16() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        int int34 = calculadora905.divInt(100, -100);
+        Assertions.assertEquals(int34, -1);
+        int int2162 = calculadora905.somaInt(500, 250);
+        Assertions.assertEquals(int2162, 750);
+    }
+
+    @Test
+    public void test17() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        double double241 = calculadora905.divDouble(-10.953, -10.953);
+        Assertions.assertEquals(double241, 1.0);
+    }
+
+    @Test
+    public void test18() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean87 = calculadora905.maior(-500, 1000);
+        Assertions.assertEquals(boolean87, false);
+        boolean boolean8171 = calculadora905.and(true, false);
+        Assertions.assertEquals(boolean8171, false);
+    }
+
+    @Test
+    public void test15() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        double double8294 = calculadora905.subDouble(1.99, -10.953);
+        Assertions.assertEquals(double8294, 12.943);
+        java.lang.String string9072 = calculadora905.toLowCase("vAlEu");
+        Assertions.assertEquals(string9072, "valeu");
+        boolean boolean87 = calculadora905.maior(-500, 1000);
+        Assertions.assertEquals(boolean87, false);
+    }
+
+    @Test
+    public void test19() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        java.lang.String string8581 = calculadora905.toLowCase("IpsUm");
+        Assertions.assertEquals(string8581, "ipsum");
+    }
+
+    @Test
+    public void test20() throws Throwable {
+        Calculadora calculadora905 = new Calculadora();
+        int int9750 = calculadora905.mulInt(-1000, 100);
+        Assertions.assertEquals(int9750, -100000);
+        double double241 = calculadora905.divDouble(-10.953, -10.953);
+        Assertions.assertEquals(double241, 1.0);
+        double double4734 = calculadora905.mulDouble(0.0, 0.0);
+        Assertions.assertEquals(double4734, 0.0);
+    }
+}
